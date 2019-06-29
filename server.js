f4c41e620c4b9eb1b9e905f008b27e360ce9f644
Wanderrful <wanderrful@gmail.com>
2019-06-29 11:50:01 -0500

    Dealing with Heroku path directory mismatch

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 // Render the React client as the root route 
 app.use("/", (req, res) => {
-	res.sendFile(path.join(__dirname + "client/build/index.html"));
+	res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 // Create the Twitch Bot and define its behavior
